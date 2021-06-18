81c3d19b47c34b767c82401c78ff4a2aaee5bf3b
hychoi <hayeong28@naver.com>
2021-06-18 14:06:38 +0900

    [INIT] set initial setting create-react-app template pwa

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import * as serviceWorkerRegistration from './serviceWorker/serviceWorkerRegistration';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = document.getElementById("root");
+const renderApp = () => (
+  <>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </>
 );
+ReactDOM.render(renderApp(), root);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
+    require("./components/App");
+    ReactDOM.render(renderApp(), root);
+  });
+}
+
 serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
